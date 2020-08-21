5e71c739a968b0da80b4695c69c811ee5a26ff13
HJK9810 <ruby45342@gamail.com>
2020-08-21 16:43:39 +0900

    복잡한 코드 함수로 뺴내기

--- src/honux/calender/Calender.java
+++ src/honux/calender/Calender.java
@@ -2,9 +2,13 @@ package honux.calender;
 import java.util.*;
 
 public class Calender {
-
-	public static void main(String[] args) {
-		
+	
+	private final int[] MAX_Days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_Days[month-1];
+	}
+	public void printSampleCalender() {
 		System.out.println("Hello, Calender!");
 		
 		System.out.println("일   월  화     수    목   금   토");
@@ -13,13 +17,17 @@ public class Calender {
 		System.out.println("8  9  10  11 12 13 14");
 		System.out.println("15 16 17  18 19 20 21");
 		System.out.println("22 23 24  25 26 27 28");
-
+	}
+	
+	public static void main(String[] args) {
+		
 		Scanner scanner = new Scanner(System.in);
+		Calender cal = new Calender();
 		System.out.println("달을 입력하세요.");
 		int month = scanner.nextInt();
-		int[] ary = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		
-		System.out.printf("%d월은 %d일 까지 있습니다.\n", month, ary[month - 1]);
+		System.out.printf("%d월은 %d일 까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		cal.printSampleCalender();
 		scanner.close();
 	}
 
