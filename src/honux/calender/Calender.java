1b6125de4a190c941df1a872af97dcb8359b0c22
HJK9810 <ruby45342@gamail.com>
2020-08-21 16:27:55 +0900

    주어진 달의 마지막 날짜 구하기

--- src/honux/calender/Calender.java
+++ src/honux/calender/Calender.java
@@ -1,9 +1,12 @@
 package honux.calender;
+import java.util.*;
 
 public class Calender {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hello, Calender!");
+		
 		System.out.println("일   월  화     수    목   금   토");
 		System.out.println("---------------------");
 		System.out.println("1  2  3   4  5  6  7");
@@ -11,6 +14,14 @@ public class Calender {
 		System.out.println("15 16 17  18 19 20 21");
 		System.out.println("22 23 24  25 26 27 28");
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("달을 입력하세요.");
+		String s1 = scanner.next();
+		int a = Integer.parseInt(s1);
+		int ary[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.printf("%d월은 %d일 까지 있습니다.", a, ary[a + 1]);
+		scanner.close();
 	}
 
 }
