da079ea5d2a6ff9092a4870b35181c55f255b032
HJK9810 <ruby45342@gamail.com>
2020-08-27 12:11:40 +0900

    error 수정

--- src/honux/calender/Calender.java
+++ src/honux/calender/Calender.java
@@ -22,15 +22,22 @@ public class Calender {
 	
 	public static void main(String[] args) {
 		
+		String PROMPT = "cal > ";
 		Scanner scanner = new Scanner(System.in);
 		Calender cal = new Calender();
-		int month = 1;
-
-		while(month != -1) {
-			System.out.println("달을 입력하세요.");
-			System.out.printf("cal > ");
-			month = scanner.nextInt();
 		
+		int month = 1;
+		while(true) {
+			System.out.println("달을 입력하세요.");
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+			if(month == -1) {
+				break;
+			}
+			
+			if(month > 12) {
+				continue;
+			}
 			System.out.printf("%d월은 %d일 까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
 		}
 		
