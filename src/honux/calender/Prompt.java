4fc41302dee73a9b32153a5352b3dc582a40a2ea
HJK9810 <ruby45342@gamail.com>
2020-09-02 15:30:50 +0900

    make UI

--- src/honux/calender/Prompt.java
+++ src/honux/calender/Prompt.java
@@ -3,8 +3,16 @@ import java.util.*;
 
 public class Prompt {
 	
+	public void printMenu() {
+		System.out.println("+----------------------+");
+		System.out.println("| 1. 일정 등록           ");
+		System.out.println("| 2. 일정 검색           ");
+		System.out.println("| 3. 달력 보기");
+		System.out.println("| h. 도움말 q. 종료");
+		System.out.println("+----------------------+");
+	}
+	
 	/**
-	 * 
 	 * week 요일명
 	 * return 0 ~ 6 ( 0 = Sunday, 6 = Saturday)
 	 */
@@ -21,27 +29,22 @@ public class Prompt {
 	}
 
 	public void runPropt() {
+		
+		printMenu();
+		
 		Scanner scanner = new Scanner(System.in);
 		Calender cal = new Calender();
 		
-		int month = 1;
-		int year = 1;
+	
+		
 		while(true) {
-			System.out.println("년을 입력하세요. (exit : -1)");
-			System.out.print("Year > ");
-			year = scanner.nextInt();
-			if(year == -1)
-				break;
-			
-			System.out.println("달을 입력하세요.");
-			System.out.print("Month > ");
-			month = scanner.nextInt();
-			if(month > 12 || month < 1) {
-				System.out.println("잘못된 월을 입력하셨습니다.\n");
-				continue;
-			}
-						
-			cal.printCalender(year, month);
+			System.out.println("명령 (1, 2, 3, h, q)");
+			String cmd = scanner.next();
+			if(cmd.equals("1")) cmdRegistor();
+			else if(cmd.equals("2")) cmdSearch();
+			else if(cmd.equals("3")) cmdCal(scanner, cal);
+			else if(cmd.equals("h")) printMenu();
+			else if(cmd.equals("q")) break;	
 		}
 		
 		System.out.println("Have a nice day!");
@@ -49,6 +52,35 @@ public class Prompt {
 	}
 
 	
+	private void cmdCal(Scanner s, Calender c) {
+		// TODO Auto-generated method stub
+		int month = 1;
+		int year = 1;	
+		System.out.println("년을 입력하세요. (exit : -1)");
+			System.out.print("Year > ");
+			year = s.nextInt();
+
+			System.out.println("달을 입력하세요.");
+			System.out.print("Month > ");
+			month = s.nextInt();
+			if(month > 12 || month < 1) {
+				System.out.println("잘못된 월을 입력하셨습니다.\n");
+				return;
+			}
+						
+			c.printCalender(year, month);
+	}
+
+	private void cmdSearch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void cmdRegistor() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	public static void main(String[] args) {
 		Prompt p = new Prompt();
 		p.runPropt();
